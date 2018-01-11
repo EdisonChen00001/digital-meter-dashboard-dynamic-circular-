74430e9083dd60058691ac6e56ce1c4209935d05
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-10 22:45:59 -0500

    dynamic button

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ function changeDigitalValue() {
     document.getElementById("firstValue").innerHTML = parseInt((Math.random())*100)+"%";
     document.getElementById("secondValue").innerHTML = parseInt((Math.random())*100)+"%";
     document.getElementById("thirdValue").innerHTML = parseInt((Math.random())*100)+"%";
+
+
 }
