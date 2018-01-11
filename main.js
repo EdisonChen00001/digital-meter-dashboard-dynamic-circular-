8b239f389966392a087a0a2828358a893aa83aaf
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-10 23:38:18 -0500

    add dyanmic strokedasbarray

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function changeDigitalValue() {
-    document.getElementById("firstValue").innerHTML = parseInt((Math.random())*100)+"%";
-    document.getElementById("secondValue").innerHTML = parseInt((Math.random())*100)+"%";
-    document.getElementById("thirdValue").innerHTML = parseInt((Math.random())*100)+"%";
+    var fisrtRandomValue,secondRandomValue,thirdRandomValue;
+    fisrtRandomValue = parseInt((Math.random())*100);
+    secondRandomValue = parseInt((Math.random())*100);
+    thirdRandomValue = parseInt((Math.random())*100);
+    document.getElementById("firstValue").innerHTML = fisrtRandomValue +"%";
+    document.getElementById("secondValue").innerHTML = secondRandomValue +"%";
+    document.getElementById("thirdValue").innerHTML = thirdRandomValue+"%";
 
+    var fisrtElement = document.getElementById("firstStrokeValue");
+    var secondElement = document.getElementById("secondStrokeValue");
+    var thirdElement = document.getElementById("thirdStrokeValue");
 
+    fisrtElement.style.strokeDasharray = fisrtRandomValue+",100";
+    secondElement.style.strokeDasharray = secondRandomValue+",100";
+    thirdElement.style.strokeDasharray = thirdRandomValue+",100";
 }
