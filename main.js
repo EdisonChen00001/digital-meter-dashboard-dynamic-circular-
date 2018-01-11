a096cab45d69f547e62b930dd8da17cd33fd0146
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-10 23:48:10 -0500

    dyanmic draw path and line

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ function changeDigitalValue() {
     secondElement.style.strokeDasharray = secondRandomValue+",100";
     thirdElement.style.strokeDasharray = thirdRandomValue+",100";
 }
+
+
+
+var element = document.getElementById("animpath");
+    var pathLength = element.getTotalLength();
+    
+    element.style.strokeDasharray = pathLength;
+    element.style.strokeDashoffset = pathLength;
+    
+    function animateRoute(e, len) 
+    {
+      // Each step we decrement the dash offset
+      len -= 10;
+      if (len < 0)
+        len = 0;  // clamp to minimum 0
+    
+      element.style.strokeDashoffset = len;
+    
+      // We need to stop looping when the length gets to 0
+      if (len > 0) {
+        // Do another step
+        setTimeout(function() { animateRoute(e, len); }, 1000);
+      }
+    }
+    
+    animateRoute(element, pathLength);
